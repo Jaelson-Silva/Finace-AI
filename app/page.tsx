ca219962f91cfd95c665ef46fcccb0a8a952f803
Jaelson Silva <jaelson.silva@stoneridge.com>
2024-11-05 13:17:52 -0300

    chore: add tailwing prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 
-export default function Home() {
-  return <h1>hello word</h1>
+const Home = () => {
+  return <div className="flex items-center justify-center w-screen">
+    <h1 className="text-red-500 p-5">Hello Word</h1>
+  </div>
 };
+
+export default Home;
 
