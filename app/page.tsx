79ceb24039168d82f752995d67d1a18d5d1edc3e
Jaelson Silva <jaelson.silva@stoneridge.com>
2024-11-05 17:18:24 -0300

    feat: creation login and add aurhentication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const Home = () => {
-  return <div className="flex items-center justify-center w-screen">
-    <h1 className="text-red-500 p-5">Hello Word</h1>
-  </div>
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
 };
 
 export default Home;
-
